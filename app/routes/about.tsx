c37128b4f34dc112dfc47d7dd4f930818fde6660
Justin Morrison <57382686+morrisonak@users.noreply.github.com>
2024-12-27 19:11:32 -0600

    Create about.tsx

---
+++ app/routes/about.tsx
@@ -0,0 +1,12 @@
+export default function About() {
+  return (
+    <section className="px-4 py-12 mx-auto max-w-7xl sm:px-6 lg:px-24">
+      <div className="text-center">
+        <h2 className="text-3xl font-bold text-neutral-800 sm:text-4xl">About Us</h2>
+        <p className="mt-4 text-gray-600">
+          We’re redefining telegraphy for the modern era, combining its rich history with cutting-edge technology.
+        </p>
+      </div>
+    </section>
+  );
+}
