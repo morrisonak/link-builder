38578dcc8ebe7b52f19ade9f0957818932b13987
Justin Morrison <57382686+morrisonak@users.noreply.github.com>
2024-12-27 19:01:20 -0600

    Update index.tsx

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,12 +1,12 @@
 // src/routes/index.tsx
 import { Link } from "@remix-run/react";
-import Nav from "~/components/nav";
+
 import Section from "~/components/section";
 
 export default function Index() {
   return (
     <>
-    <Nav />
+  
     <Section />
     </>
 );
